0ff6b4ab108c5f4aaf88a1099ce5c46cddf3b500
sk1ovo <15336442898@163.com>
2025-09-09 21:02:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ get_current_dayofweek = lambda action: (
 )
 
 
-SLEEPTIME = 0.1  # 每次抢座的间隔
+SLEEPTIME = 0.05  # 每次抢座的间隔
 ENDTIME = "22:16:00"  # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = True  # 是否有滑块验证
